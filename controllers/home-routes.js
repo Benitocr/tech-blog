2fce989da5e2816e5ad96171aeebcaf9960aa176
Benito <benitocavazosr@yahoo.com>
2022-02-19 18:38:49 -0600

    homepageview

---
+++ controllers/home-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        include: [
+            {
+              model: Comment,
+              attributes: ['id', 'comment_text', 'post_id', 'user_id', 'createdAt'],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+    })
+    .then(dbPostData => {
+      // pass a single post object into the homepage template
+      // console.log(dbPostData[0]);
+      const posts = dbPostData.map(post => post.get({plain: true}));
+      res.render('homepage', {
+        posts
+        // loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+  
+});
+
+module.exports = router;
